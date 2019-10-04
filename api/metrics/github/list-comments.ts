8fffcc1de79225a4b39a0e83e1585082de9c0513
Jasper De Moor <jasperdemoor@gmail.com>
2019-10-04 21:56:54 +0200

    cleanup fetch a lil, for some reasom failed though

--- api/metrics/github/list-comments.ts
+++ api/metrics/github/list-comments.ts
@@ -2,29 +2,18 @@ import fetch from 'cross-fetch';
 import urlJoin from 'url-join';
 
 import { REPO_OWNER, REPO_NAME } from '../../../constants';
-import { GITHUB_AUTH } from '../constants';
-import * as base64 from '../utils/base64';
 
 type PostCommentOptions = {
   issueNumber: string;
 };
 
 export default async function listComments(options: PostCommentOptions): Promise<any> {
-  let headers = {
-    Authorization: 'Basic ' + base64.encode(GITHUB_AUTH)
-  };
-
   let url = urlJoin('https://api.github.com/repos', REPO_OWNER, REPO_NAME, 'issues', options.issueNumber, 'comments');
-  let res = await fetch(url, {
-    method: 'GET',
-    headers
-  });
+  let res = await (await fetch(url)).json();
 
   if (!res.ok) {
     throw new Error('Failed to get comments: ' + res.statusText);
   }
-
-  res = await res.json();
 
   console.log(`Got comments ${url}`);
 
